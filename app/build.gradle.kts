21c1f9608947ec8e63393c1e338c6fbc60c55e38
Angel Velasquez <angelsoft071@gmail.com>
2024-01-19 16:06:24 -0500

    Create Fragments and activate Navigation Component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,10 @@ android {
 }
 
 dependencies {
+    // NavComponent
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
