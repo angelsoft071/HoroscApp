e8c669f273aca29c050b8caed151d35436875741
Angel Velasquez <angelsoft071@gmail.com>
2024-01-20 15:12:22 -0500

    Add states with StateFlow for HoroscopeDetail, enable  internet permission

--- app/src/main/java/com/angelsoft/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,9 +3,14 @@ package com.angelsoft.horoscapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.angelsoft.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -18,6 +23,44 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        initUI()
+    }
+
+    private fun initUI() {
+        initUiState()
+    }
+
+    private fun initUiState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        is HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+        showProgressBar()
+    }
+
+    private fun errorState() {
+        hideProgressBar()
+    }
+
+    private fun successState() {
+        hideProgressBar()
+    }
+
+    private fun showProgressBar() {
+        binding.progressBar.isVisible = true
+    }
+
+    private fun hideProgressBar() {
+        binding.progressBar.isVisible = false
     }
 }
