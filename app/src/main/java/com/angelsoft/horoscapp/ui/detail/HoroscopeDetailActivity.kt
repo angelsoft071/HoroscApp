b4a9dc767973dc02f57235341df683e794955d54
Angel Velasquez <angelsoft071@gmail.com>
2024-01-20 12:27:34 -0500

    Add navigation to HoroscopeDetailActivity with safeArgs

--- app/src/main/java/com/angelsoft/horoscapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -1,9 +1,9 @@
 package com.angelsoft.horoscapp.ui.detail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.angelsoft.horoscapp.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.navArgs
 import com.angelsoft.horoscapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,9 +12,12 @@ class HoroscopeDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
+    private val args: HoroscopeDetailActivityArgs by navArgs()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        args.type
     }
 }
--- app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -4,14 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.angelsoft.horoscapp.databinding.FragmentHoroscopeBinding
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Aquarius
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Aries
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Cancer
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Capricorn
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Gemini
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Leo
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Libra
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Pisces
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Sagittarius
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Scorpio
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Taurus
+import com.angelsoft.horoscapp.domain.model.HoroscopeInfo.Virgo
+import com.angelsoft.horoscapp.domain.model.HoroscopeModel
 import com.angelsoft.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -36,7 +49,23 @@ class HoroscopeFragment : Fragment() {
 
     private fun initList() {
         horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
-            Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+            val type = when (it) {
+                Aquarius -> HoroscopeModel.Aquarius
+                Aries -> HoroscopeModel.Aries
+                Cancer -> HoroscopeModel.Cancer
+                Capricorn -> HoroscopeModel.Capricorn
+                Gemini -> HoroscopeModel.Gemini
+                Leo -> HoroscopeModel.Leo
+                Libra -> HoroscopeModel.Libra
+                Pisces -> HoroscopeModel.Pisces
+                Sagittarius -> HoroscopeModel.Sagittarius
+                Scorpio -> HoroscopeModel.Scorpio
+                Taurus -> HoroscopeModel.Taurus
+                Virgo -> HoroscopeModel.Virgo
+            }
+            findNavController().navigate(
+                HoroscopeFragmentDirections.actionHoroscopeFragmentToHoroscopeDetailActivity(type)
+            )
         })
 
         binding.rvHoroscope.apply {
