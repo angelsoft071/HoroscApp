4748357bed5ca37a44f99313789c0d19b0ae7aa9
Angel Velasquez <angelsoft071@gmail.com>
2024-01-20 11:48:41 -0500

    Add lambda function to HoroscopeFragment

--- app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,17 +1,16 @@
 package com.angelsoft.horoscapp.ui.horoscope
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.angelsoft.horoscapp.databinding.FragmentHoroscopeBinding
 import com.angelsoft.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,7 +35,9 @@ class HoroscopeFragment : Fragment() {
     }
 
     private fun initList() {
-        horoscopeAdapter = HoroscopeAdapter()
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = {
+            Toast.makeText(context, getString(it.name), Toast.LENGTH_SHORT).show()
+        })
 
         binding.rvHoroscope.apply {
             layoutManager = GridLayoutManager(context, 2)
--- app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,15 +1,29 @@
 package com.angelsoft.horoscapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.AccelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.angelsoft.horoscapp.databinding.ItemHoroscopeBinding
 import com.angelsoft.horoscapp.domain.model.HoroscopeInfo
 
 class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val binding = ItemHoroscopeBinding.bind(view)
-    fun render(horoscopeInfo: HoroscopeInfo) {
+    fun render(horoscopeInfo: HoroscopeInfo, onItemSelected: (HoroscopeInfo) -> Unit) {
         val context = binding.ivHoroscope.context
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvHoroscopeTitle.text = context.getString(horoscopeInfo.name)
+        binding.parent.setOnClickListener {
+            startRotation(binding.ivHoroscope, callback = { onItemSelected(horoscopeInfo) })
+        }
+    }
+
+    private fun startRotation(view: View, callback: () -> Unit) {
+        view.animate().apply {
+            duration = 300
+            interpolator = AccelerateInterpolator()
+            rotationBy(360f)
+            withEndAction { callback() }
+            start()
+        }
     }
 }
