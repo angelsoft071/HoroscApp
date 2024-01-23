1f6cd9d379ff9544afe2ae4ff00ff57c11c33958
Angel Velasquez <angelsoft071@gmail.com>
2024-01-23 08:25:17 -0500

    Camera permission management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,15 +57,25 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // DaggerHilt
-    implementation("com.google.dagger:hilt-android:2.48")
-    kapt("com.google.dagger:hilt-compiler:2.48")
+    val daggerHiltVersion = "2.48"
+    implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerHiltVersion")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
 
     // Retrofit interceptor
     implementation("com.squareup.okhttp3:logging-interceptor:4.3.1")
+
+    // CameraX
+    val cameraVersion = "1.3.1"
+    implementation ("androidx.camera:camera-core:$cameraVersion")
+    implementation ("androidx.camera:camera-camera2:$cameraVersion")
+    implementation ("androidx.camera:camera-lifecycle:$cameraVersion")
+    implementation ("androidx.camera:camera-view:$cameraVersion")
+    implementation ("androidx.camera:camera-extensions:$cameraVersion")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
