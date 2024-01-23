be34ffd6e4a2c0316bfac18b14887db5cd1f1cab
Angel Velasquez <angelsoft071@gmail.com>
2024-01-23 07:57:41 -0500

    Introduce RandomCardProvider to get predictions, add OnSwipeTouchListener to get predictions when swipe over the cards

--- app/src/main/java/com/angelsoft/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/angelsoft/horoscapp/ui/luck/LuckFragment.kt
@@ -1,6 +1,8 @@
 package com.angelsoft.horoscapp.ui.luck
 
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,13 +16,18 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.angelsoft.horoscapp.R
 import com.angelsoft.horoscapp.databinding.FragmentLuckBinding
+import com.angelsoft.horoscapp.ui.core.listener.OnSwipeTouchListener
+import com.angelsoft.horoscapp.ui.provider.RandomCardProvider
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
+
+    @Inject lateinit var randomCardProvider: RandomCardProvider
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,11 +35,41 @@ class LuckFragment : Fragment() {
     }
 
     private fun initUI() {
+        preparePrediction()
         initListeners()
     }
 
+    private fun preparePrediction() {
+        val luckyModel = randomCardProvider.getLucky()
+        luckyModel?.let { luck ->
+            binding.tvLucky.text = getString(luck.text)
+            binding.ivLuckyCard.setImageResource(luck.image)
+            binding.btnShare.setOnClickListener { shareResult(getString(luck.text)) }
+        }
+    }
+
+    private fun shareResult(prediction: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, prediction)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun initListeners() {
-        binding.ivRoulette.setOnClickListener { spinRoulette() }
+        binding.ivRoulette.setOnTouchListener(object : OnSwipeTouchListener(requireContext()){
+            override fun onSwipeRight() {
+                spinRoulette()
+            }
+
+            override fun onSwipeLeft() {
+                spinRoulette()
+            }
+        })
     }
 
     private fun spinRoulette() {
